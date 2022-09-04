18e68acaeb3c0a7378046cf10e3f8e9d43049cd8
KritagyaChopra <85625671+KritagyaChopra@users.noreply.github.com>
2022-09-04 22:14:39 +0530

    Update Java End-of-file.java

--- Java End-of-file.java
+++ Java End-of-file.java
@@ -7,6 +7,7 @@ import java.util.regex.*;
 public class Solution {
 
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         int x = 0;
         while(sc.hasNext()){
